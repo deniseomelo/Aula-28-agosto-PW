a9f71391748c85b8bb8c05c8864787efd58f9948
deniseomelo <108363094+deniseomelo@users.noreply.github.com>
2022-08-24 10:51:12 -0300

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,11 @@
+/*    SENAC - TADS - Programação web 
+         Exemplo de função anonima*/
+
+var nomeTela = "Denise";
+
+var mensagem = (nomeEntrada)=>{
+    console.log("Bom dia Sr. " + nomeEntrada + ", Seja bem vindos")
+
+}
+
+mensagem(nomeTela);
